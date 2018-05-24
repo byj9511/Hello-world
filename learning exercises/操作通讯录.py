d42babd019f52e7d07708c5f1fd0f1ea2e76d208
byj9511 <38412504+byj9511@users.noreply.github.com>
2018-05-24 10:15:28 +0800

    Update 操作通讯录.py

--- learning exercises/操作通讯录.py
+++ learning exercises/操作通讯录.py
@@ -1,7 +1,6 @@
 from sys import exit
-import time
 
-address_list = {}
+address_list = {'byj':123}
 print('欢迎进入通讯录程序')
 print("1：查询联系人资料")
 print("2：插入新的联系人")
@@ -11,25 +10,26 @@ print("4: 推出通讯录程序")
 
 def user_action_1():
     name = input("请输入联系人姓名：")
-    if name in address_list:
-        print(name + ':' + address_list[name])
-    else:
+    try:
+        print(name + ':' ,address_list[name])
+    except KeyError:
         print("您所查找的联系人不存在")
 
 
 def user_action_2():
     name = input("请输入联系人姓名：")
-    if name in address_list:
+    try:
         print("你输入的用户已存在")
-    else:
-        adress_list[name] = input("请输入用户联系电话：")
+        print(name + ':' , address_list[name])
+    except:
+        address_list[name] = input("请输入用户联系电话：")
 
 
 def user_action_3():
     name = input("请输入联系人姓名：")
-    if name in address_list:
+    try:
         del address_list[name]
-    else:
+    except:
         print("您所查找的联系人不存在")
 
 
